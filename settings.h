fb0c53fa518e35127d5024a633bdc35d88bd23c8
curly60e <55191248+curly60e@users.noreply.github.com>
2022-01-07 21:48:33 -0300

    Update settings.h

--- settings.h
+++ settings.h
@@ -3,11 +3,11 @@
 
 //Weather Settings
 #define CITY_ID "5128581" //New York City https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "none" //use your own API key :)
+#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
-#define WEATHER_UPDATE_INTERVAL 99 //must be greater than 5, measured in minutes
+#define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
 //NTP Settings
 #define NTP_SERVER "pool.ntp.org"
 #define GMT_OFFSET_SEC 3600 * -5 //New York is UTC -5
